8ef9a13b2e73a4b231688c87f9b2e88f7d8a7c32
kevinchien556 <tamamacool@gmail.com>
2021-10-17 11:25:54 +0800

    add axios practice

---
+++ crawler/app.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+let stockCode = "2330";
+let today = "20211017";
+let format = "json";
+
+// "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=" + format + "&date=" + today + "&stockNo=" + stockCode
+// `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=${format}&date=${today}&stockNo=${stockCode}`
+
+// axios
+//   .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//     params: {
+//       response: format,
+//       date: today,
+//       stockNo: stockCode,
+//     },
+//   })
+//   .then((res) => {
+//     // HTTP response
+//     console.log(res.data);
+//   })
+//   .catch((e) => {
+//     console.error("發生錯誤啦", e);
+//   });
+
+  let promiseAxios=axios
+  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+      response: format,
+      date: today,
+      stockNo: stockCode,
+    },
+  });
+
+  (async () => {
+    try{
+        let dataAwait= await promiseAxios;
+    console.log("await讀檔正確",dataAwait.data);
+    }
+    catch(e){
+        console.error(e);
+    } 
+})();
